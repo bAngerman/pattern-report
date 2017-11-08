973d8a2680013c7ddfb110a05a37635fec24bf7f
bAngerman <Brendan.angerman@gmail.com>
2017-11-07 20:24:13 -0700

    updating code example

--- js/main.js
+++ js/main.js
@@ -4,6 +4,25 @@
 (function(exports, $) {
   'use strict';
 
+  /* Begin Model Constructor */
 
+  var Vehicle = function(attrs) {
+    var attr;
+    
+    // defaults:
+    this.make = 'Honda';
+    this.model = 'Civic';
+    this.year = '1999';
+    this.mileage = '20000';
+
+    // mixin initial values
+    if (attrs) {
+      for (attr in attrs) {
+        this[attr] = attrs[attr];
+      }
+    }
+  };
+
+  /* End  Model Constructor */
 
 }(this, jQuery));
