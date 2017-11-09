8571aaf7fc12a73eb407aa9bcd8e970134ca1dc7
bAngerman <Brendan.angerman@gmail.com>
2017-11-08 17:29:43 -0700

    model done more or less

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 /**
+ * This wont do anything... I just want the js I write to be linted.
  * 
  */
 (function(exports, $) {
@@ -22,6 +23,19 @@
       }
     }
   };
+
+  Vehicle.prototype.set = function(attr, val) {
+    this[attr] = val;
+  };
+
+  Vehicle.prototype.get = function(attr) {
+    return this[attr];
+  };
+
+  var defaultCar = new Vehicle();
+  defaultCar.set('mileage', '39999');
+
+  var myCar = new Vehicle({make: 'Volkswagen', model: 'Jetta', year: '2003', mileage: '310000'});
 
   /* End  Model Constructor */
 
