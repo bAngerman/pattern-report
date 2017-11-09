97b8c9406c4a542e372633144d9bfced9dfe8d14
bAngerman <Brendan.angerman@gmail.com>
2017-11-08 18:40:26 -0700

    view + controller examples

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
   var Vehicle = function(attrs) {
     var attr;
     
-    // defaults:
+    // defaults
     this.make = 'Honda';
     this.model = 'Civic';
     this.year = '1999';
@@ -32,11 +32,60 @@
     return this[attr];
   };
 
+  /* End Model Constructor */
+
+
   var defaultCar = new Vehicle();
   defaultCar.set('mileage', '39999');
 
   var myCar = new Vehicle({make: 'Volkswagen', model: 'Jetta', year: '2003', mileage: '310000'});
 
-  /* End  Model Constructor */
+  /* Begin Vehicle View stuff */
+  
+  Vehicle.prototype.render = function($tar) {
+    // empty the target div
+    $tar.empty();
+
+    // create dom elements to hold the information
+    var container = document.createElement('div'),
+
+        itemMake = document.createElement('span'),
+        itemModel = document.createElement('span'),
+        itemYear = document.createElement('span'),
+        itemMileage = document.createElement('span'),
+
+        make = document.createTextNode('Make: ' + this.get('make')),
+        model = document.createTextNode('Model: ' + this.get('model')),
+        year = document.createTextNode('Year: ' + this.get('year')),
+        mileage = document.createTextNode('Mileage: ' + this.get('mileage'));
+
+    container.className = 'border rounded';
+
+    itemMake.appendChild(make);
+    itemModel.appendChild(model);
+    itemYear.appendChild(year);
+    itemMileage.appendChild(mileage);
+
+    container.appendChild(itemMake);
+    container.appendChild(itemModel);
+    container.appendChild(itemYear);
+    container.appendChild(itemMileage);
+
+    // append the dom element into the page
+    $tar.append(container);
+  };
+
+  /* End Vehicle View stuff */
+
+
+  /* Start Controller stuff */
+
+  $('#defaultCar').on('click', function(e) {
+    var $tar = $('#defaultCarContainer');
+    defaultCar.render($tar);
+  });
+
+  /* End Controller stuff */
+  
 
 }(this, jQuery));
